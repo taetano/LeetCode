97fb8f9d21ae5d3870228c7bfd830ec219ca79a5
taetano <144678214+taetano@users.noreply.github.com>
2024-01-05 11:25:02 +0900

    Time: 23 ms (9.28%), Space: 42.9 MB (71.50%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,38 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         // 정렬 후 문자열 비교
-        s = s.chars()
-            .sorted()
-            .collect(StringBuilder::new,
-                     StringBuilder::appendCodePoint,
-                     StringBuilder::append)
-            .toString();
+//         s = s.chars()
+//             .sorted()
+//             .collect(StringBuilder::new,
+//                      StringBuilder::appendCodePoint,
+//                      StringBuilder::append)
+//             .toString();
         
-        t = t.chars()
-            .sorted()
-            .collect(StringBuilder::new,
-                     StringBuilder::appendCodePoint,
-                     StringBuilder::append)
-            .toString();
+//         t = t.chars()
+//             .sorted()
+//             .collect(StringBuilder::new,
+//                      StringBuilder::appendCodePoint,
+//                      StringBuilder::append)
+//             .toString();
         
-        return s.equals(t);
+//         return s.equals(t);
         // mpa counting
-            
+        if (s.length() != t.length()) return false;
+        
+        Map<Character,Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char sKey = s.charAt(i);
+            char tKey = t.charAt(i);
+            map.put(sKey, map.getOrDefault(sKey, 0) + 1);
+            map.put(tKey, map.getOrDefault(tKey, 0) - 1);
+        }
+        
+        for (Map.Entry<Character,Integer> it : map.entrySet()) {
+            if (it.getValue() != 0) return false;
+        }
+        
+        return true;
         // array counting
     }
 }
