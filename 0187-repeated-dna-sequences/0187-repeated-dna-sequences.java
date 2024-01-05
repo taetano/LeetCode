bc630174cfe01e4977c1c8afb65d3e8ef18516bc
taetano <144678214+taetano@users.noreply.github.com>
2024-01-05 11:52:55 +0900

    Time: 17 ms (72.98%), Space: 53.7 MB (11.49%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,20 +1,32 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         // #1 map count
+//         if (s.length() <= 10) return Collections.emptyList();
+        
+//         Map<String, Integer> map = new HashMap<>();
+//         for (int i = 0; i <= s.length() - 10; i++) {
+//             String key = s.substring(i, i + 10);
+//             map.put(key, map.getOrDefault(key, 0) + 1);
+//         }
+        
+//         List<String> ans = new ArrayList<>();
+//         for (Map.Entry<String, Integer> it : map.entrySet()) {
+//             if (it.getValue() > 1) ans.add(it.getKey());
+//         }
+        
+//         return ans;
+        // #2 set count
         if (s.length() <= 10) return Collections.emptyList();
         
-        Map<String, Integer> map = new HashMap<>();
+        Set<String> candi = new HashSet<>();
+        Set<String> set = new HashSet<>();
         for (int i = 0; i <= s.length() - 10; i++) {
-            String key = s.substring(i, i + 10);
-            map.put(key, map.getOrDefault(key, 0) + 1);
+            String str = s.substring(i, i + 10);
+            if (candi.contains(str)) {
+                set.add(str);
+            } else candi.add(str);
         }
         
-        List<String> ans = new ArrayList<>();
-        for (Map.Entry<String, Integer> it : map.entrySet()) {
-            if (it.getValue() > 1) ans.add(it.getKey());
-        }
-        
-        return ans;
-        // #2 set count
+        return new ArrayList(set);
     }
 }
