cf845d8aad092d7276bb1b702500709b74c3a0aa
taetano <144678214+taetano@users.noreply.github.com>
2024-01-05 11:29:25 +0900

    Time: 7 ms (33.75%), Space: 43.2 MB (68.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // 정렬 후 문자열 비교
+        // #1 정렬 후 문자열 비교
 //         s = s.chars()
 //             .sorted()
 //             .collect(StringBuilder::new,
@@ -16,23 +16,41 @@ class Solution {
 //             .toString();
         
 //         return s.equals(t);
-        // mpa counting
+        
+        // #2 mpa counting
+//         if (s.length() != t.length()) return false;
+        
+//         Map<Character,Integer> map = new HashMap<>();
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             char sKey = s.charAt(i);
+//             char tKey = t.charAt(i);
+//             map.put(sKey, map.getOrDefault(sKey, 0) + 1);
+//             map.put(tKey, map.getOrDefault(tKey, 0) - 1);
+//         }
+        
+//         for (Map.Entry<Character,Integer> it : map.entrySet()) {
+//             if (it.getValue() != 0) return false;
+//         }
+        
+//         return true;
+        
+        // #3 array counting
         if (s.length() != t.length()) return false;
         
-        Map<Character,Integer> map = new HashMap<>();
+        int[] counter = new int[26];
         
         for (int i = 0; i < s.length(); i++) {
-            char sKey = s.charAt(i);
-            char tKey = t.charAt(i);
-            map.put(sKey, map.getOrDefault(sKey, 0) + 1);
-            map.put(tKey, map.getOrDefault(tKey, 0) - 1);
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            counter[sChar - 'a']++;
+            counter[tChar - 'a']--;
         }
         
-        for (Map.Entry<Character,Integer> it : map.entrySet()) {
-            if (it.getValue() != 0) return false;
+        for (int num : counter) {
+            if (num != 0) return false;
         }
         
         return true;
-        // array counting
     }
 }
