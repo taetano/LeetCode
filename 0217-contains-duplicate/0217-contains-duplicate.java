3b937c68bfa9bed725dd98255ab93167d87e92fd
taetano <144678214+taetano@users.noreply.github.com>
2024-01-10 13:09:18 +0900

    Time: 7 ms (98.33%), Space: 60.3 MB (13.77%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,19 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
+        // #using Map
+        // Map<Integer,Integer> map = new HashMap<>();
+        // for (int each : nums) {
+        //     if (map.containsKey(each)) return true;
+        //     map.put(each, 0);
+        // }
+        // return false;
+        
+        // # using set
+        Set<Integer> set = new HashSet<>();
         for (int each : nums) {
-            if (map.containsKey(each)) return true;
-            map.put(each, 0);
+            if (!set.add(each)) return true;
         }
+        
         return false;
     }
 }
