523007a92c2c10e9b299b1b2d7e6d72150ea0b12
taetano <144678214+taetano@users.noreply.github.com>
2024-01-10 13:00:40 +0900

    Time: 17 ms (21.49%), Space: 45.2 MB (12.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,16 +1,37 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] arr = new int[26];
+        // # using array
+//         int[] arr = new int[26];
+//         for (char each : ransomNote.toCharArray()) {
+//             arr[each - 'a']++;
+//         }
+        
+//         for (char each : magazine.toCharArray()) {
+//             arr[each - 'a']--;
+//         }
+        
+//         for (int each : arr) {
+//             if (each > 0) return false;
+//         }
+        
+//         return true;
+        
+        // using hashMap
+        
+        Map<Character, Integer> map = new HashMap<>();
         for (char each : ransomNote.toCharArray()) {
-            arr[each - 'a']++;
+            map.put(each, map.getOrDefault(each, 0) + 1);
         }
         
         for (char each : magazine.toCharArray()) {
-            arr[each - 'a']--;
+            if (map.containsKey(each)) {
+                map.put(each, map.get(each) - 1);
+            }
         }
         
-        for (int each : arr) {
-            if (each > 0) return false;
+        
+        for (int val : map.values()) {
+            if (val > 0) return false;
         }
         
         return true;
