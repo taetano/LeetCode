e2204df57d23af61f7e2f6d0f4ffad8f49d6c7e5
Choi Chanho <144678214+taetano@users.noreply.github.com>
2024-01-11 11:09:09 +0900

    Time: 0 ms (100.00%), Space: 57.6 MB (13.40%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,12 +1,28 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         // 배열 2개
-        int[] ans = new int[nums.length];
+//         int[] ans = new int[nums.length];
         
-        for (int i = 0; i < nums.length; i++) {
-            ans[(i + k) % nums.length] = nums[i];
-        }
+//         for (int i = 0; i < nums.length; i++) {
+//             ans[(i + k) % nums.length] = nums[i];
+//         }
 
-        System.arraycopy(ans, 0, nums, 0, ans.length);
+//         System.arraycopy(ans, 0, nums, 0, ans.length);
+
+        // space O(1)  ex) [1, 2, 3, 4, 5, 6, 7]
+        k %= nums.length;    // 0 <= k <= 10^5
+        reverse(nums, 0, nums.length - 1); // [7, 6, 5, 4, 3, 2, 1]
+        reverse(nums, 0, k - 1);           // [5, 6, 7, 4, 3, 2, 1]
+        reverse(nums, k, nums.length - 1); // [5, 6, 7, 1, 2, 3, 4]
+    }
+    
+    void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
+        }
     }
 }
