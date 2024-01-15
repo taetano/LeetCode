7aa66133eaa1bddfdebede12ec327dfbd5327414
Choi Chanho <144678214+taetano@users.noreply.github.com>
2024-01-15 11:56:37 +0900

    Time: 3 ms (75.72%), Space: 43 MB (62.81%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().trim();
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) left++;
+            else if(!Character.isLetterOrDigit(s.charAt(right))) right--;
+            else if (s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        
+        return true;
+    }
+}
