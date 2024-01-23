b18bf6be5687c8706a190918c7fc8707c06f8914
Choi Chanho <144678214+taetano@users.noreply.github.com>
2024-01-23 18:04:49 +0900

    Time: 3 ms (84.11%), Space: 42.7 MB (79.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,54 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // #1 정렬 후 문자열 비교
-//         s = s.chars()
-//             .sorted()
-//             .collect(StringBuilder::new,
-//                      StringBuilder::appendCodePoint,
-//                      StringBuilder::append)
-//             .toString();
-        
-//         t = t.chars()
-//             .sorted()
-//             .collect(StringBuilder::new,
-//                      StringBuilder::appendCodePoint,
-//                      StringBuilder::append)
-//             .toString();
-        
-//         return s.equals(t);
-        
-        // #2 mpa counting
-//         if (s.length() != t.length()) return false;
-        
-//         Map<Character,Integer> map = new HashMap<>();
-        
-//         for (int i = 0; i < s.length(); i++) {
-//             char sKey = s.charAt(i);
-//             char tKey = t.charAt(i);
-//             map.put(sKey, map.getOrDefault(sKey, 0) + 1);
-//             map.put(tKey, map.getOrDefault(tKey, 0) - 1);
-//         }
-        
-//         for (Map.Entry<Character,Integer> it : map.entrySet()) {
-//             if (it.getValue() != 0) return false;
-//         }
-        
-//         return true;
-        
-        // #3 array counting
         if (s.length() != t.length()) return false;
-        
-        int[] counter = new int[26];
-        
-        for (int i = 0; i < s.length(); i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
-            counter[sChar - 'a']++;
-            counter[tChar - 'a']--;
+        int[] cnt = new int[26];
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            cnt[c - 'a']++;
         }
         
-        for (int num : counter) {
-            if (num != 0) return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (--cnt[c - 'a'] < 0) return false;
         }
         
         return true;
