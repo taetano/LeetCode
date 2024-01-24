57383f68a5012f0ad728df7f74a88826c9f9991c
Choi Chanho <144678214+taetano@users.noreply.github.com>
2024-01-24 09:52:06 +0900

    Time: 0 ms (100.00%), Space: 57.4 MB (37.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,28 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        // 배열 2개
-//         int[] ans = new int[nums.length];
+        k %= nums.length;
         
-//         for (int i = 0; i < nums.length; i++) {
-//             ans[(i + k) % nums.length] = nums[i];
-//         }
-
-//         System.arraycopy(ans, 0, nums, 0, ans.length);
-
-        // space O(1)  ex) [1, 2, 3, 4, 5, 6, 7]
-        k %= nums.length;    // 0 <= k <= 10^5
-        reverse(nums, 0, nums.length - 1); // [7, 6, 5, 4, 3, 2, 1]
-        reverse(nums, 0, k - 1);           // [5, 6, 7, 4, 3, 2, 1]
-        reverse(nums, k, nums.length - 1); // [5, 6, 7, 1, 2, 3, 4]
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
     }
     
-    void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            int tmp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = tmp;
-            left++;
-            right--;
+    void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            swap(nums, l++, r--);
         }
+    }
+    
+    void swap(int[] nums, int l, int r) {
+        int tmp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = tmp;
     }
 }
