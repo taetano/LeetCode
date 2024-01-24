bd6130f5bd57e86f088229f09ea330ef9e8ac1df
Choi Chanho <144678214+taetano@users.noreply.github.com>
2024-01-24 10:05:38 +0900

    Time: 13 ms (98.30%), Space: 49.5 MB (95.41%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,15 +1,24 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> duple = new HashSet<>();
-        Set<String> ansMaterials = new HashSet<>();
+        Set<Integer> words = new HashSet<>();
+        Set<Integer> doubleWords = new HashSet<>();
+        List<String> ans = new ArrayList<>();
+        int[] map = new int[26];
+        map['C' - 'A'] = 1;
+        map['G' - 'A'] = 2;
+        map['T' - 'A'] = 3;
         
         for (int i = 0; i + 10 <= s.length(); i++) {
-            String dna = s.substring(i, i + 10);
-            if (!duple.add(dna)) {
-                ansMaterials.add(dna);
+            int v = 0;
+            for (int j = i; j < i + 10; j++) {
+                v <<= 2;
+                v |= map[s.charAt(j) - 'A'];
+            }
+            if (!words.add(v) && doubleWords.add(v)) {
+                ans.add(s.substring(i, i + 10));
             }
         }
         
-        return new ArrayList(ansMaterials);
+        return ans;
     }
 }
