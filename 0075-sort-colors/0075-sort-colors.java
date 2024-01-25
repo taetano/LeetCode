d62c0e6beb2c5c935332af86d723dddfcc506c08
Choi Chanho <144678214+taetano@users.noreply.github.com>
2024-01-25 10:24:12 +0900

    Time: 0 ms (100.00%), Space: 41.4 MB (65.06%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,17 +1,5 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int[] cnt = new int[3];
-        
-        for (int each : nums) {
-            cnt[each]++;
-        }
-        
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < cnt[0]; j++) nums[j] = 0;
-            
-            for (int j = cnt[0]; j < cnt[0] + cnt[1]; j++) nums[j] = 1;
-            
-            for (int j = cnt[0] + cnt[1]; j < cnt[0] + cnt[1] + cnt[2]; j++) nums[j] = 2;
-        }
+        Arrays.sort(nums);
     }
 }
