b8f5da714789d7b0b2b7f51169320b20c53ee6fc
Choi Chanho <144678214+taetano@users.noreply.github.com>
2024-01-26 10:23:17 +0900

    Time: 0 ms (100.00%), Space: 42.5 MB (6.35%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,47 +1,21 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // # 값을 기준으로 정렬이 되어 있는 탐색구역을 제한하여 찾는다.
-//         int left = 0, right = nums.length -1;
-        
-//         while (left <= right) {
-//             int mid = left + (right - left) / 2;
-//             if (nums[mid] == target) return mid;
-            
-//             if (nums[left] <= nums[mid]) {
-//                 if (nums[left] <= target && target < nums[mid]) {
-//                     right = mid - 1;
-//                 } else {
-//                     left = mid + 1;
-//                 }
-//             } else {
-//                 if (nums[right] >= target && target > nums[mid]) {
-//                     left = mid + 1;
-//                 } else {
-//                     right = mid - 1;
-//                 }
-//             }
-//         }
-        
-        
-//         return -1;
-        
-        // #2 
-        int lo = 0, hi = nums.length - 1;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] > nums[hi]) lo = mid + 1;
-            else hi = mid;
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] > nums[r]) l = m + 1;
+            else r = m;
         }
         
-        int rot = lo;
-        lo = 0; hi = nums.length - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            int realMid = (mid + rot) % nums.length;
-            if (nums[realMid] == target) return realMid;
+        int k = l;
+        l = 0; r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            int realM = (m + k) % nums.length;
+            if (nums[realM] == target) return realM;
             
-            if (nums[realMid] < target) lo = mid + 1;
-            else hi = mid - 1;
+            if (nums[realM] < target) l = m + 1;
+            else r = m - 1;
         }
         
         return -1;
