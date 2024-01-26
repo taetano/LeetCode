a541e06f41ca75b4b7fdbef101d857a70fa7eec5
Choi Chanho <144678214+taetano@users.noreply.github.com>
2024-01-26 09:50:13 +0900

    Time: 3 ms (56.27%), Space: 45.6 MB (32.76%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,24 +1,12 @@
 class Solution {
-
     public boolean canJump(int[] nums) {
-        if (nums.length == 1) return true;
-        
-        boolean[] arr = new boolean[nums.length];
-        arr[0] = true;
-        for (int i = 0; i < nums[0] && i + 1 < nums.length; i++) {
-            arr[i + 1] = true;
+        int reachable = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > reachable) return false;
+            reachable = Math.max(reachable, nums[i] + i);
         }
         
-        for (int i = 1; i < nums.length; i++) {
-            if (!arr[i]) return false;
-            for (int j = i; j < i + nums[i] && j + 1 < nums.length; j++) {
-                arr[j + 1] = true;
-            }
-            
-            if (arr[arr.length - 1]) return true;
-        }
-        
-        return false;
+        return true;
     }
 
 }
