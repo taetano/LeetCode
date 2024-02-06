0654aad64bd10df97b7dec23766f448adb05501e
Choi Chanho <144678214+taetano@users.noreply.github.com>
2024-02-06 10:00:17 +0900

    Time: 1 ms (98.77%), Space: 41.6 MB (19.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,11 @@
 class Solution {
     public boolean isValid(String s) {
         Deque<Character> stack = new LinkedList<>();
-        
         for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                stack.addLast(c);
-                continue;
-            }
-            else if (stack.isEmpty()) return false;
-            
-            char peek = stack.peekLast();
-            if (c == ')' && peek != '(') return false;
-            else if (c == ']' && peek != '[') return false;
-            else if (c == '}' && peek != '{') return false;
-            else stack.removeLast();
-            
+            if (c == '(') stack.addLast(')');
+            else if (c == '[') stack.addLast(']');
+            else if (c == '{') stack.addLast('}');
+            else if (stack.isEmpty() || stack.removeLast() != c) return false;
         }
         
         return stack.isEmpty();
